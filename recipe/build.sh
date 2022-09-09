2ae4fe801715a21acf102383109d665fd98e01c7
Julien Schueller <schueller@phimeca.com>
2022-09-09 22:42:23 +0200

    Write versioned symlinks

--- recipe/build.sh
+++ recipe/build.sh
@@ -42,6 +42,9 @@ cmake -LAH -G "Ninja" ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_FIND_FRAMEWORK=LAST \
   -DCMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib \
+  -DINSTALL_BINDIR=lib/qt6/bin \
+  -DINSTALL_PUBLICBINDIR=bin \
+  -DINSTALL_LIBEXECDIR=lib/qt6 \
   -DINSTALL_DOCDIR=share/doc/qt6 \
   -DINSTALL_ARCHDATADIR=lib/qt6 \
   -DINSTALL_DATADIR=share/qt6 \
@@ -73,3 +76,10 @@ qttools;\
 qttranslations" \
   ..
 cmake --build . --target install
+
+cd ${PREFIX}
+mkdir -p bin
+for links in ${SRC_DIR}/build/qt*/user_facing_tool_links.txt
+do
+  while read _line; do ln -sf $_line; done < ${links}
+done
