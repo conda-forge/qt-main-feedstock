89eb42fa4bbe8c963d4f9be81759453b1265e27b
Julien Schueller <schueller@phimeca.com>
2022-09-27 16:39:47 +0200

    win32: INSTALL_ARCHDATADIR=lib/qt6

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -21,6 +21,7 @@ cmake -LAH -G "Ninja" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DINSTALL_ARCHDATADIR=lib/qt6 ^
     -DINSTALL_INCLUDEDIR=include/qt6 ^
     -DINSTALL_MKSPECSDIR=lib/qt6/mkspecs ^
     -DINSTALL_DATADIR=share/qt6 ^
