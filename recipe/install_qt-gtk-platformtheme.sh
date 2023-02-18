0e20d1fec1928641b8c5c2489439664fc6f240e1
Mark Harfouche <mark.harfouche@gmail.com>
2023-02-18 09:44:30 -0500

    Delete install_qt-gtk-platformtheme.sh

--- recipe/install_qt-gtk-platformtheme.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-
-ls -lah
-ls -lah qt-build
-
-mkdir -p "$PREFIX/lib/qt5/plugins/platformthemes"
-install \
-    qt-build/qtbase/plugins/platformthemes/libqgtk3.so \
-    "$PREFIX/plugins/platformthemes/libqgtk3.so"
