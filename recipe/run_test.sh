7c420820c63ae8300b1cc9fceb9ed4e3a55de98f
Mark Harfouche <mark.harfouche@gmail.com>
2023-02-25 19:45:06 -0500

    Update recipe/run_test.sh
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+
+# If qt6.conf is not part of the package, it won't work when installed side by side with Qt5.
+# See https://github.com/conda-forge/qt-main-feedstock/issues/99
 test -f ${PREFIX}/bin/qt6.conf
 
 test "${HOST}" = "aarch64-conda-linux-gnu" && exit 0
