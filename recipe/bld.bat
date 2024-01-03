0862d2d2ae377c49aa9e5afab5777203b42fe620
Julien Schueller <schueller@phimeca.com>
2024-01-03 14:53:26 +0100

    quick

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,16 +3,6 @@
 mkdir %LIBRARY_PREFIX%\lib\cmake\tiff\include
 
 set "MODS=qtbase"
-set "MODS=%MODS%;qtdeclarative"
-set "MODS=%MODS%;qtimageformats"
-set "MODS=%MODS%;qtmultimedia"
-set "MODS=%MODS%;qtshadertools"
-set "MODS=%MODS%;qtsvg"
-set "MODS=%MODS%;qttools"
-set "MODS=%MODS%;qttranslations"
-set "MODS=%MODS%;qt5compat"
-set "MODS=%MODS%;qtwebchannel"
-set "MODS=%MODS%;qtwebsockets"
 
 :: Support systems with neither capable OpenGL (desktop mode) nor DirectX 11 (ANGLE mode) drivers
 :: https://github.com/ContinuumIO/anaconda-issues/issues/9142
@@ -63,7 +53,6 @@ if errorlevel 1 exit 1
 
 :: link public exes with suffix (mklink does not play well with new .conda zip format)
 copy %LIBRARY_PREFIX%\lib\qt6\bin\qmake.exe %LIBRARY_PREFIX%\bin\qmake6.exe
-copy %LIBRARY_PREFIX%\lib\qt6\bin\windeployqt.exe %LIBRARY_PREFIX%\bin\windeployqt6.exe
 if errorlevel 1 exit 1
 
 :: You can find the expected values of these files in the log
